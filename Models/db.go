80abbaa79454d4e9de19fdf264abeb463105dae8
Tkex555 <bahamutthero@gmail.com>
2025-10-17 10:00:16 -0500

    se corrigio el usuario

--- Models/db.go
+++ Models/db.go
@@ -2,7 +2,7 @@ package Models
 
 import (
 	"database/sql"
-	
+
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -11,7 +11,7 @@ var DB *sql.DB
 func InitDB() error {
 	var err error
 	// Cambia los datos de conexión según tu entorno
-	dsn := "usuario:contraseña@tcp(127.0.0.1:3306)/banco?parseTime=true"
+	dsn := "root:@tcp(127.0.0.1:3306)/banco?parseTime=true"
 	DB, err = sql.Open("mysql", dsn)
 	if err != nil {
 		return err
